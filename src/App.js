d00eb5281e84e10069da129ec4eaa555f1c1d6c0
Br-yk <bryk.defi@gmail.com>
2023-09-03 00:12:37 +0100

    little added functionalities

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+import { useState } from 'react';
 import './App.css';
+import { Button, FormControl, Input, InputLabel } from '@mui/material';
 
 function App() {
+  const [todos, setTodos] = useState(['Write Code','Buy a book','Play the guitar','Rent a house']);
+  const [input, setInput] = useState('');
+  console.log('🤩',input)
+  const addTodo = (event) => {
+    event.preventDefault()
+    console.log('😍')
+    setTodos([...todos, input]);
+    setInput('');
+    console.log(todos)
+  }
   return (
     <div className="App">
       <div>
-        <input />
-        <button>Add ToDo</button>
+      <FormControl>
+        <InputLabel>Add Task</InputLabel>
+        <Input value={input} onChange={event => setInput(event.target.value)}  />
+        <Button variant='contained' type='submit' onClick={addTodo} disabled={!input}>Add ToDo</Button>
+      </FormControl>
       </div>
       <div>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+          {todos.map(todo => (
+            <li>{todo}</li>
+          ))}
         </ul>  
       </div>   
     </div>
