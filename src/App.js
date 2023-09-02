f8ee09ecdcee700c79b5c4d5f4b7158fef42abf0
Br-yk <bryk.defi@gmail.com>
2023-09-02 22:18:43 +0100

    app setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <input />
+        <button>Add ToDo</button>
+      </div>
+      <div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>  
+      </div>   
+    </div>
+  );
+}
+
+export default App;
